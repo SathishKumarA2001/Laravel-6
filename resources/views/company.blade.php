84ef64ef9449dc2d7ec726f1985a58da4a0e7e85
Sathish Kumar <chesathish5201@gmail.com>
2022-08-12 13:44:44 +0530

    Layouts integration

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Theme Made By www.w3schools.com -->
-  <title>Bootstrap</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+@extends('layouts.layout')
 
-</head>
-<body>
-
+@section('company')
 <div class="jumbotron text-center">
   <h1>Company</h1> 
   <p>Axiom Consultancy</p> 
@@ -31,9 +18,5 @@
     @endif
   @endforeach
   </div>
-
-
 </div>
-
-</body>
-</html>
+@endsection
